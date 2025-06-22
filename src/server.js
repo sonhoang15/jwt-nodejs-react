47406b515faad77c5e6ab43fa2d22214c66e4c87
Hoang Son <yani.gmaing123@gmail.com>
2025-06-22 15:13:55 +0700

    start up

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import configViewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+require("dotenv").config();
+
+
+const app = express();
+
+configViewEngine(app);
+
+initWebRoutes(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log("backend runing " + PORT)
+})
