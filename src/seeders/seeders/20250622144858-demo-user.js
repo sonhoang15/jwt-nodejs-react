2725fd2eff205b2ccb06ed9ceb8513fb256294f5
Hoang Son <yani.gmaing123@gmail.com>
2025-06-22 21:53:03 +0700

    ORM

---
+++ src/seeders/seeders/20250622144858-demo-user.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Users', [{
+      username: 'John Doe1',
+      email: 'fake1@gmail.com',
+      password: '123456'
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 import bodyParser from "body-parser";
+// import connection from "./config/connectDB"
 require("dotenv").config();
 
 
@@ -11,7 +12,12 @@ configViewEngine(app);
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// connection();
+
 initWebRoutes(app);
+
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
