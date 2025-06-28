ea61c7120548094a89e708d91b747e6eedc460ae
Hoang Son <yani.gmaing123@gmail.com>
2025-06-28 17:43:09 +0700

    authenticated users

--- src/middleware/JWTAction.js
+++ src/middleware/JWTAction.js
@@ -15,16 +15,72 @@ const createJWT = (payload) => {
 
 const verifyToken = (token) => {
     let key = process.env.JWT_KEY
-    let data = null
+    let decoded = null
     try {
-        let decoded = jwt.verify(token, key)
-        data = decoded
+        decoded = jwt.verify(token, key)
+
     } catch (error) {
         console.log(err);
     }
     return data;
 }
 
+const checkUserJWT = (req, res, next) => {
+    let cookie = req.cookie;
+    if (cookie && cookie.jwt) {
+        let token = cookie.jwt
+        let decoded = verifyToken(token)
+        if (decoded) {
+            req.user = decoded
+            next();
+        } else {
+            return res.status(401), json({
+                EC: -1,
+                DT: '',
+                EM: 'not authenticated the user'
+            })
+        }
+
+    } else {
+        return res.status(401), json({
+            EC: -1,
+            DT: '',
+            EM: 'not authenticated the user'
+        })
+    }
+
+}
+
+const checkUserPermission = (req, res, next) => {
+    if (req.user) {
+        let email = req.user.email
+        let roles = req.user.groupWithRoles.Roles
+        let currentUrl = req.path
+        if (!roles || roles.length === 0) {
+            return res.status(403), json({
+                EC: -1,
+                DT: '',
+                EM: `you don't permission to access this resource ...`
+            })
+        }
+        let canAccess = roles.some(item => item.url === currentUrl)
+        if (canAccess === true) {
+            next()
+        } else {
+            return res.status(403), json({
+                EC: -1,
+                DT: '',
+                EM: `you don't permission to access this resource ...`
+            })
+        }
+    } else {
+        return res.status(401), json({
+            EC: -1,
+            DT: '',
+            EM: 'not authenticated the user'
+        })
+    }
+}
 module.exports = {
-    createJWT, verifyToken
+    createJWT, verifyToken, checkUserJWT, checkUserPermission
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,13 +2,18 @@ import express from "express";
 import apiController from "../controller/apiController";
 import userController from "../controller/userController";
 import groupController from "../controller/groupController";
+import { checkUserJWT, checkUserPermission } from "../middleware/JWTAction"
 const router = express.Router();
+
+// const checkUserLogin = (req, res, next) => {
+
+// }
 
 const initApiRoutes = (app) => {
     router.post("/register", apiController.handleRegister);
     router.post("/login", apiController.handleLogin);
 
-    router.get("/user/read", userController.read);
+    router.get("/user/read", checkUserJWT, checkUserPermission, userController.read);
     router.post("/user/create", userController.create);
     router.put("/user/update", userController.update);
     router.delete("/user/delete", userController.DeleteUser);
