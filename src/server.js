0178f93cb01520556e1eb1173ee49b61f21452d3
Hoang Son <yani.gmaing123@gmail.com>
2025-07-04 21:49:59 +0700

    fix deploy

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import cookieParser from 'cookie-parser'
 // import connection from "./config/connectDB"
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 
 // app.use(cors({
