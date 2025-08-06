92e02d2b7934f783d01258341b6972860d34ff14
Hoang Son <yani.gmaing123@gmail.com>
2025-08-06 15:27:59 +0700

    fix CORS

--- src/config/cors.js
+++ src/config/cors.js
@@ -6,21 +6,21 @@ const configcors = (app) => {
         'https://jwt-react-hook-hoang-sns-projects.vercel.app',
         'https://jwt-react-hook-git-master-hoang-sns-projects.vercel.app'
     ];
-    app.use((req, res, next) => {
 
+    app.use((req, res, next) => {
         const origin = req.headers.origin;
 
         if (allowedOrigins.includes(origin)) {
             res.setHeader('Access-Control-Allow-Origin', origin);
+            res.setHeader('Vary', 'Origin');
         }
 
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-        // ✅ Xử lý preflight requestz
         if (req.method === 'OPTIONS') {
-            return res.sendStatus(204); // No Content
+            return res.sendStatus(204);
         }
 
         next();
